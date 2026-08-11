684dca1c9292dd860139bb51b6f7b67625e4064a
PaoloB91 <paolo@pscompanysrl.com>
2026-08-11 17:39:39 +0200

    Aggiornamento app lavanderia 2026-08-11

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker: rende l'app apribile anche senza rete.
    CACHE_VERSION va incrementata a ogni pubblicazione (lo fa deploy.sh). */
-const CACHE_VERSION = "lavanderia-v3";
+const CACHE_VERSION = "lavanderia-v4";
 const FILE = [
   "./",
   "./index.html",
